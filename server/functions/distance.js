1f97b2cbaaf822912adf1cf8dda42037ff3eea56
femoxmed <femoxmed@gmail.com>
2019-04-03 00:14:22 -1100

    =distance calculator

--- server/functions/distance.js
+++ server/functions/distance.js
@@ -10,7 +10,14 @@ const toRadians = (value) => {
 // using haversine formular
 const distance = (lat1, lon1, lat2, lon2) => {
 
-    let R = 6371e3;
+    /*
+    Q = latitude
+    Y  = longtitude
+    CQ , CY change in latutude, longtitude respectively
+    R = the earth mean radius 6.371 km or  or 6.371e3m
+    D = Distance
+    */
+    let R = 6371e3; // this is will give result in metres, but in km =(6371)
 
     let Q1 = toRadians(lat1);
 
@@ -27,10 +34,13 @@ const distance = (lat1, lon1, lat2, lon2) => {
     let C = 2 * Math.atan2(Math.sqrt(A), Math.sqrt(1 - A));
 
     let D = R * C;
-
+    // console.log(D);
     return D;
 
 
 }
 
 export default distance
+
+
+// distance(18.643190, 3.254760, 8.559120, 3.365850)
