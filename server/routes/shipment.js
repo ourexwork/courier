e4904fe8b6aeb0d94103eb007464f1f6952ac9b9
femoxmed <femoxmed@gmail.com>
2019-04-06 02:19:50 -1100

    =tracking admin transaction history

--- server/routes/shipment.js
+++ server/routes/shipment.js
@@ -8,45 +8,44 @@ const authenticateUser = require('../middleware/authenticateUser');
 const getLocation = require('../functions/getLocation');
 
 // this route enables a user to add a new shipment
-router.post('/', authenticateUser, async (req, res) => {
-  // if we have a user
-  if (req.user) {
-    // validate the request body
-    const { error } = validateShipment(req.body);
-    if (error) return res.status(400).send({ error: error.details[0].message });
+router.post('/', authenticateUser, async(req, res) => {
+    // if we have a user
+    if (req.user) {
+        // validate the request body
+        const { error } = validateShipment(req.body);
+        if (error) return res.status(400).send({ error: error.details[0].message });
 
-    // pick all shipment properties from the request body
-    const newShipment =
-      _.pick[
-        (req.body, 'name', 'description', 'weight', 'quantity', 'shipmentMode')
-      ];
-    // get address call the geo-location api and store into recent address
-    const pickUpAddress = getLocation(req.body.pickUpAddress).then(
-      location => location
-    );
-    const deliveryAddress = getLocation(req.body.deliveryAddress).then(
-      location => location
-    );
+        // pick all shipment properties from the request body
+        const newShipment =
+            _.pick(req.body, ['name', 'description', 'weight', 'quantity', 'shipmentMode']);
 
-    // get the receiver's details
-    const receiver = {
-      name: req.body.receiverName,
-      email: req.body.receiverEmail,
-      phoneNumber: req.body.receiverPhoneNumber
-    };
+        // get address call the geo-location api and store into recent address
+        const pickUpAddress = getLocation(req.body.pickUpAddress).then(
+            location => location
+        );
+        const deliveryAddress = getLocation(req.body.deliveryAddress).then(
+            location => location
+        );
 
-    // note adress is a schema of addresses
-    // store the shipment in the database
-    const shipment = new shipment({
-      ...newShipment,
-      pickUpAddress,
-      deliveryAddress,
-      receiver
-    });
+        // get the receiver's details
+        const receiver = {
+            name: req.body.receiverName,
+            email: req.body.receiverEmail,
+            phoneNumber: req.body.receiverPhoneNumber
+        };
 
-    await shipment.save();
+        // note adress is a schema of addresses
+        // store the shipment in the database
+        const shipment = new shipment({
+            ...newShipment,
+            pickUpAddress,
+            deliveryAddress,
+            receiver
+        });
 
-    res.send({ message: 'shipment saved!!' });
-  }
+        await shipment.save();
+
+        res.send({ message: 'shipment saved!!' });
+    }
 });
 module.exports = router;
