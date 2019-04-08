21ad3771839c4cff01d5cd56747e3c585d2337b4
femoxmed <femoxmed@gmail.com>
2019-04-07 20:31:03 -1100

    =priceschema and save schema

--- server/routes/shipment.js
+++ server/routes/shipment.js
@@ -11,8 +11,10 @@ const authenticateUser = require('../middleware/authenticateUser');
 // this route enables a user to add a new shipment
 router.post('/', authenticateUser, userShipment);
 
+// this routes returns the object to be saved to the client to review
 router.post('/getprice', authenticateUser, getdetailsAndPrice);
 
+// this route saves the data to databse both shipment and price
 router.post('/save-shipment', authenticateUser, saveShipment);
 
 module.exports = router;
