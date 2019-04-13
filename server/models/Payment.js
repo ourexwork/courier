0a98a93b7139cb71a421a6a925666e3d5e616cb4
femoxmed <femoxmed@gmail.com>
2019-04-13 00:49:22 -1100

    =refactor shipment queries

--- server/models/Payment.js
+++ server/models/Payment.js
@@ -34,4 +34,4 @@ const paymentSchema = new mongoose.Schema({
 
 const Payment = mongoose.model('Payment', paymentSchema);
 
-module.exports = { Payment };
+module.exports = { Payment, paymentSchema };
