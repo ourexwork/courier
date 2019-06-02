804db2ac4c1d5d33636713dff814c47d952fbb0c
knowsarzehmeh <osarzeh@gmail.com>
2019-06-02 09:29:19 +0100

    Created the Preloader,PrivateRoutes and PublicRoutes component

--- client/src/redux/actions/auth.js
+++ client/src/redux/actions/auth.js
@@ -1,56 +1,47 @@
 //import config from 'config';
 import axios from 'axios';
 
-export const login = (auth) => ({
-    type: 'LOGIN_USER',
-    auth
-
-})
-
+export const login = auth => ({
+  type: 'LOGIN_USER',
+  auth
+});
 
 export const startLogin = (userData = {}) => {
+  return dispatch => {
+    const { username = '', password = '' } = userData;
 
-    return (dispatch) => {
+    const user = { username, password };
 
-        const {
-            username = '',
-                password = '',
+    return axios
+      .post('api/users/login', user)
+      .then(snapshot => {
+        console.log(snapshot);
 
+        console.log(snapshot.headers.xauthtoken);
 
-        } = userData;
-
-
-        const user = { username, password };
-
-        return axios.post('api/users/login', user).then((snapshot) => {
-                console.log(snapshot)
-
-                console.log(snapshot.headers.xauthtoken)
-
-                localStorage.setItem('x-auth-token', snapshot.headers.xauthtoken)
-                dispatch(login(snapshot.data))
-                return { success: true }
-            }).catch((e) => {
-
-                const user = { _id: '', username: '', email: '', error: e.response.data.error }
-                dispatch(login(user))
-                return { success: false }
-                // console.log(error.response.data)
-
-            })
-            // try {
-            //     const callBackEnd = await axios.post('api/users/login', user);
-            //     dispatch(login(callBackEnd.data))
-            // } catch (error) {
-            //     if (error.response) {
-            //                 console.log(error.response.data)
-            //                 return error.response.data
-            //             }
-            // }
-
-
-
-    }
-
-
-}
+        localStorage.setItem('x-auth-token', snapshot.headers.xauthtoken);
+        dispatch(login(snapshot.data));
+        return { success: true };
+      })
+      .catch(e => {
+        const user = {
+          _id: '',
+          username: '',
+          email: '',
+          error: e.response.data.error
+        };
+        dispatch(login(user));
+        return { success: false };
+        // console.log(error.response.data)
+      });
+    // try {
+    //     const callBackEnd = await axios.post('api/users/login', user);
+    //     dispatch(login(callBackEnd.data))
+    // } catch (error) {
+    //     if (error.response) {
+    //                 console.log(error.response.data)
+    //                 return error.response.data
+    //             }
+    // }
+  };
+};
