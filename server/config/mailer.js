9a4bea55491f5ede71c4bb0d9830e0334f099b45
femoxmed <femoxmed@gmail.com>
2019-07-12 04:35:19 -1100

    =mailing issue

--- server/config/mailer.js
+++ server/config/mailer.js
@@ -1,5 +1,5 @@
-'use strict';
-const nodemailer = require('nodemailer');
+"use strict";
+const nodemailer = require("nodemailer");
 
 // auth: {
 //     user: 'femoxmed@gmail.com',
@@ -7,39 +7,39 @@ const nodemailer = require('nodemailer');
 //   }
 
 // the sender's user and pass
-const user = 'femoxmed@gmail.com';
-const pass = 'omotayo123*';
+const user = "femoxmed@gmail.com";
+const pass = "omotayo123*";
 //  create the transporter
 const transporter = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-        user,
-        pass
-    },
-    tls: {
-        rejectUnauthorized: false
-    }
+  service: "Gmail",
+  auth: {
+    user,
+    pass
+  },
+  tls: {
+    rejectUnauthorized: false
+  }
 });
 
 //  create the body of the mail
-const sendMail = async(recipient, token) => {
-    // here we configure our host
-    const url = `http://localhost:9191/api/user/confirmation/${token}`;
+const sendMail = async (recipient, token) => {
+  // here we configure our host http://localhost:9191/api/user/confirmation/${token}
+  const url = `http://rilxpress.com/api/user/confirmation/${token}`;
 
-    const mail = {
-        from: user,
-        to: recipient,
-        subject: 'Confirm Account',
-        html: `<h1>Welcome User</h1><br>Please click this link to confirm your account: <br> <br> <br><a style="border-radius: 5px solid; text-decoration: none; background-color: blue; padding: 15px; color: white; margin: 0 auto; " href="${url}">Click Here </a>`
-    };
+  const mail = {
+    from: user,
+    to: recipient,
+    subject: "Confirm Account",
+    html: `<h1>Welcome User</h1><br>Please click this link to confirm your account: <br> <br> <br><a style="border-radius: 5px solid; text-decoration: none; background-color: blue; padding: 15px; color: white; margin: 0 auto; " href="${url}">Click Here </a>`
+  };
 
-    try {
-        await transporter.sendMail(mail);
-        console.log('mail sent');
-    } catch (error) {
-        console.log(error);
-        // throw new Error(error);
-    }
+  try {
+    await transporter.sendMail(mail);
+    console.log("mail sent");
+  } catch (error) {
+    console.log(error);
+    // throw new Error(error);
+  }
 };
 
 module.exports = { sendMail };
